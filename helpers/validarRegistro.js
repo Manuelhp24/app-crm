4db0a6c2b1adbf55f009332f3615da6af7a18e4d
Manuelhp24 <manuel2412hm@gmail.com>
2025-09-13 14:54:32 -0500

    (develot) Inicio de validación con expreciones regulares.

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,8 @@
+let inputs = document.getElementsByClassName("formulario__entrada");
+let arregloInputs = [...inputs];
+console.log(inputs);
+console.log(arregloInputs);
+
+arregloInputs.map((input) => {
+    console.log(input);
+});
